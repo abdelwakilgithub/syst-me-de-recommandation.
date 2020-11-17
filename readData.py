5ccb2ba7a5d26166d06035e69100f7af1a05f9d4
abdelwakilgithub <37119853+abdelwakilgithub@users.noreply.github.com>
2020-11-17 13:38:06 +0100

    Update readData.py

--- readData.py
+++ readData.py
@@ -3,9 +3,9 @@ import numpy as np
 
 excel = pd.read_excel("Data/Reviews-Asmi.xlsx")
 table_rating = excel.rename(
-    columns={'reviewerName': 'Users', 'categories': 'Products'})
+    columns={'reviewerName': 'User', 'categories': 'Product'})
 table_rating_new = table_rating.groupby(
-    ['Users', 'Products']).agg({'rating': ['mean']})
-matrice_rating = pd.pivot_table(table_rating_new, index=["Users"], columns=[
-                                'Products'], values=['rating']).fillna(0)
+    ['User', 'Product']).agg({'rating': ['mean']})
+matrice_rating = pd.pivot_table(table_rating_new, index=["User"], columns=[
+                                'Product'], values=['rating']).fillna(0)
 # print(matrice_rating)
